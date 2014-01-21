32ec5d6dfa6788a2dd050a830934eee3e2da8581
oliverswitzer <oliverswitzer@gmail.com>
2014-01-20 22:33:54 -0500

    count_sentences now working using regex

--- count_sentences/src/countSentences.js
+++ count_sentences/src/countSentences.js
@@ -1,3 +1,9 @@
-function countSentences() {
-  // code goes here
-}
+function countSentences(string) {
+	var split_array = string.split(/\./);
+	var size = split_array.length;
+	if (size == 1) {
+		return 0
+	} else {
+		return size;
+	};
+};
